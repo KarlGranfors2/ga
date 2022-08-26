7a0610d5f4756484b8a49bd46ed6a4c7fb70da14
Karl Granfors <kargra2003@skola.goteborg.se>
2022-08-26 10:27:55 +0200

    Created main program "shitman".

---
+++ shitman.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+struct Deck {
+    int deck[52];
+    
+    Deck() {
+        for(int i = 0; i < 52; i++) {
+            deck[i] = i;
+        }
+    }
+    
+    void blanda() {
+        shuffle(begin(this->deck), end(this->deck), mt19937{random_device{}()});
+    }
+
+
+};
+
+int main() {
+    
+    Deck deck1;
+    deck1.blanda();
+    
+    for(int i = 0; i < 52; i++) {
+        cout << deck1.deck[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
