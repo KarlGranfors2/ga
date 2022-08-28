e30c33496dace4aa7afbe3f5f0e20a4b2e10a49c
Karl Granfors <kargra2003@skola.goteborg.se>
2022-08-28 10:38:23 +0200

    Cleanup

--- shitman.cpp
+++ shitman.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 struct Deck {
     int deck[52];
+    int index;
     
     Deck() {
         for(int i = 0; i < 52; i++) {
@@ -15,6 +16,17 @@ struct Deck {
     
     void blanda() {
         shuffle(begin(this->deck), end(this->deck), mt19937{random_device{}()});
+        index = 0;
+    }
+
+    int draw() {
+        if (index > 51) {
+            blanda();
+            cout << "Reshuffled" << endl; //Not supposed to happen
+        }
+
+        index++;
+        return deck[index -1];
     }
 
 
@@ -26,7 +38,8 @@ int main() {
     deck1.blanda();
     
     for(int i = 0; i < 52; i++) {
-        cout << deck1.deck[i] << " ";
+        //cout << deck1.deck[i] << " ";
+        cout << deck1.draw() << " ";
     }
     cout << endl;
 
